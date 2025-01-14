5283345a3c58f57cd9a767a9763d7b0d1d16cbfe
ArthropodR <arthropodr@gmail.com>
2025-01-14 13:45:23 +0600

    JailPluginFV

---
+++ src/main/java/com/zitemaker/jails/commands/DelJailCommand.java
@@ -0,0 +1,39 @@
+package com.zitemaker.jails.commands;
+
+import com.zitemaker.jails.JailsPlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DelJailCommand implements CommandExecutor {
+
+    private final JailsPlugin plugin;
+
+    public DelJailCommand(JailsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("jailplugin.deljail")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            return true;
+        }
+
+        if (args.length != 1) {
+            sender.sendMessage(ChatColor.RED + "Usage: /deljail <jail name>");
+            return true;
+        }
+
+        String jailName = args[0].toLowerCase();
+        if (!plugin.getJails().containsKey(jailName)) {
+            sender.sendMessage(ChatColor.RED + "Jail '" + jailName + "' does not exist.");
+            return true;
+        }
+
+        plugin.removeJail(jailName);
+        sender.sendMessage(ChatColor.GREEN + "Jail " + ChatColor.YELLOW + jailName + ChatColor.GREEN + " has been successfully deleted.");
+        return true;
+    }
+}
---
+++ src/main/java/com/zitemaker/jails/commands/UnjailCommand.java
@@ -0,0 +1,55 @@
+package com.zitemaker.jails.commands;
+
+import com.zitemaker.jails.JailsPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.util.UUID;
+
+public class UnjailCommand implements CommandExecutor {
+
+    private final JailsPlugin plugin;
+
+    public UnjailCommand(JailsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("jailplugin.unjail")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            return true;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "Usage: /unjail <player>");
+            return true;
+        }
+
+
+        OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+
+        if (target == null || !target.hasPlayedBefore()) {
+            sender.sendMessage(ChatColor.RED + "Player not found or has never joined the server.");
+            return true;
+        }
+
+        UUID targetUUID = target.getUniqueId();
+
+        if (!plugin.isPlayerJailed(targetUUID)) {
+            sender.sendMessage(ChatColor.YELLOW + target.getName() + " is not jailed.");
+            return true;
+        }
+
+
+        plugin.unjailPlayer(targetUUID);
+
+        sender.sendMessage(ChatColor.GREEN + "Player " + target.getName() + " has been unjailed.");
+        Bukkit.broadcastMessage(ChatColor.GREEN + target.getName() + " has been unjailed.");
+        return true;
+    }
+}

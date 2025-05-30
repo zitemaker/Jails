9462690600aa4ea73fa613037484f44325a5c2c0
ArthropodR <arthropodr@gmail.com>
2025-05-30 09:44:02 +0600

    Language update and improvements to update logger

--- src/main/java/me/zitemaker/jail/commands/ConfigReload.java
+++ src/main/java/me/zitemaker/jail/commands/ConfigReload.java
@@ -1,6 +1,7 @@
 package me.zitemaker.jail.commands;
 
 import me.zitemaker.jail.JailPlugin;
+import me.zitemaker.jail.listeners.TranslationManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,19 +9,23 @@ import org.bukkit.command.CommandSender;
 
 public class ConfigReload implements CommandExecutor {
     private final JailPlugin plugin;
+    private final TranslationManager translationManager;
 
     public ConfigReload(JailPlugin plugin) {
         this.plugin = plugin;
+        this.translationManager = plugin.getTranslationManager();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String prefix = plugin.getPrefix();
+
         if (sender.hasPermission("jails.reload")) {
             plugin.reloadPluginConfig();
-            sender.sendMessage(ChatColor.GREEN + "Jails configuration reloaded!");
-            return true;
+            sender.sendMessage(prefix + " " + ChatColor.GREEN + translationManager.getMessage("config_reload_success"));
+        } else {
+            sender.sendMessage(prefix + " " + ChatColor.GREEN + translationManager.getMessage("cr_no_permission"));
         }
-        sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
         return true;
     }
 }
--- src/main/java/me/zitemaker/jail/commands/JailListCommand.java
+++ src/main/java/me/zitemaker/jail/commands/JailListCommand.java
@@ -1,27 +1,15 @@
 package me.zitemaker.jail.commands;
 
 import me.zitemaker.jail.JailPlugin;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
+import me.zitemaker.jail.listeners.TranslationManager;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,23 +17,24 @@ import java.util.List;
 import java.util.UUID;
 
 public class JailListCommand implements CommandExecutor, TabCompleter, Listener {
-
     private final JailPlugin plugin;
+    private final TranslationManager translationManager;
 
     public JailListCommand(JailPlugin plugin) {
         this.plugin = plugin;
         Bukkit.getPluginManager().registerEvents(this, plugin);
+        this.translationManager = plugin.getTranslationManager();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player player)) {
-            sender.sendMessage(ChatColor.RED + "Only players can use this command.");
+            sender.sendMessage(ChatColor.RED + translationManager.getMessage("jailed_list_only_players"));
             return true;
         }
 
         if (!player.hasPermission("jails.list")) {
-            player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            player.sendMessage(ChatColor.RED + translationManager.getMessage("jailed_list_no_permission"));
             return true;
         }
 
